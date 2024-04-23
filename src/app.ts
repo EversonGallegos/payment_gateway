b2f393060bd123554bfa32890a8ad66523262ab5
EversonGallegos <gallegoseverson@gmail.com>
2024-04-23 16:23:01 -0300

    refactor: added app inside a function

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import Fastify, { FastifyListenOptions } from 'fastify';
+import Fastify, { FastifyInstance, FastifyListenOptions } from 'fastify';
 import payablesRoutes from './modules/payables/payables.route';
 import transactionsRoutes from './modules/transactions/transactions.route';
 import { transactionSchemas } from './modules/transactions/transaction.schema';
@@ -7,72 +7,78 @@ import fastifySwagger from '@fastify/swagger';
 import fastifySwaggerUi from '@fastify/swagger-ui';
 import { payableSchemas } from './modules/payables/payables.schema';
 
-const server = Fastify({
-  logger: true
-});
 
-//Documentation
-server.register(fastifySwagger, {
-  swagger: {
-    info: {
-      title: 'Payment Gateway API',
-      description: 'Documentação da API do gateway de pagamento',
-      version: '0.1.0'
+function buildFastify (): FastifyInstance {
+
+  const server = Fastify({
+    logger: true
+  });
+  
+  //Documentation
+  server.register(fastifySwagger, {
+    swagger: {
+      info: {
+        title: 'Payment Gateway API',
+        description: 'Documentação da API do gateway de pagamento',
+        version: '0.1.0'
+      },
+      host: 'localhost:3000',
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json'],
     },
-    host: 'localhost:3000',
-    schemes: ['http'],
-    consumes: ['application/json'],
-    produces: ['application/json'],
-  },
-  hideUntagged: false,
-})
-
-server.register(fastifySwaggerUi, {
-  routePrefix: '/docs',
-  uiConfig: {
-    docExpansion: 'list',
-    deepLinking: false
-  },
-  uiHooks: {
-    onRequest: function (request, reply, next) { next() },
-    preHandler: function (request, reply, next) { next() }
-  },
-  staticCSP: true,
-  transformStaticCSP: (header) => header,
-  transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
-  transformSpecificationClone: true
-})
-
-for(const schema of [...transactionSchemas, ...payableSchemas]) {
-  server.addSchema(schema);
-}
-
-// Registering routes
-server.register(payablesRoutes, {
-  prefix: '/payable'
-})
-
-server.register(transactionsRoutes, {
-  prefix: '/transaction'
-})
-//
-
-const port: number = process.env.PORT
-? parseInt(process.env.PORT)
-: 3000;
-const host: string = process.env.HOST ?? 'localhost';
-
-const opts: FastifyListenOptions = {
-  port,
-  host
-}
-
-server.listen(opts, function (err, address) {
-  if (err) {
-    server.log.error(err)
-    process.exit(1)
+    hideUntagged: false,
+  })
+  
+  server.register(fastifySwaggerUi, {
+    routePrefix: '/docs',
+    uiConfig: {
+      docExpansion: 'list',
+      deepLinking: false
+    },
+    uiHooks: {
+      onRequest: function (request, reply, next) { next() },
+      preHandler: function (request, reply, next) { next() }
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
+    transformSpecificationClone: true
+  })
+  
+  for(const schema of [...transactionSchemas, ...payableSchemas]) {
+    server.addSchema(schema);
   }
-  server.log.info(`server listening on ${address}`)
-});
+  
+  // Registering routes
+  server.register(payablesRoutes, {
+    prefix: '/payable'
+  })
+  
+  server.register(transactionsRoutes, {
+    prefix: '/transaction'
+  })
+  //
+  
+  const port: number = 3000;
+  const host: string = '127.0.0.1';
+  
+  const opts: FastifyListenOptions = {
+    port,
+    host
+  }
+  
+  server.listen(opts, function (err, address) {
+    if (err) {
+      server.log.error(err)
+      process.exit(1)
+    }
+    server.log.info(`server listening on ${address}`)
+  });
 
-export default server;
+  return server
+}
+
+buildFastify();
+
+export default buildFastify
